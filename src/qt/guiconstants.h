0cbde67989442892ded5e8df92aec11e2ac08c4b
48thct2jtnf <48thct2jtnf@users.noreply.github.com>
2017-08-27 22:56:04 +0100

    Update guiconstants.h

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -49,9 +49,9 @@ static const int MAX_URI_LENGTH = 255;
 /* Number of frames in spinner animation */
 #define SPINNER_FRAMES 35
 
-#define QAPP_ORG_NAME "PIVX"
-#define QAPP_ORG_DOMAIN "pivx.org"
-#define QAPP_APP_NAME_DEFAULT "PIVX-Qt"
-#define QAPP_APP_NAME_TESTNET "PIVX-Qt-testnet"
+#define QAPP_ORG_NAME "Phore"
+#define QAPP_ORG_DOMAIN "phore.io"
+#define QAPP_APP_NAME_DEFAULT "Phore-Qt"
+#define QAPP_APP_NAME_TESTNET "Phore-Qt-testnet"
 
 #endif // BITCOIN_QT_GUICONSTANTS_H
