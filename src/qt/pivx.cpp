9973777ec1dc3300610004b844653ecf68c7ba01
48thct2jtnf <48thct2jtnf@users.noreply.github.com>
2017-08-28 17:36:27 +0100

    Update pivx.cpp

--- src/qt/pivx.cpp
+++ src/qt/pivx.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2014-2015 The Dash developers
 // Copyright (c) 2015-2017 The PIVX developers
+// Copyright (c) 2017 The Phore developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -165,7 +166,7 @@ void DebugMessageHandler(QtMsgType type, const QMessageLogContext& context, cons
 }
 #endif
 
-/** Class encapsulating PIVX Core startup and shutdown.
+/** Class encapsulating Phore Core startup and shutdown.
  * Allows running startup and shutdown in a different thread from the UI thread.
  */
 class BitcoinCore : public QObject
